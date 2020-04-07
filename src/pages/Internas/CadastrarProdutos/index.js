313fc42ef094cddc38e052e5706bd4b986f17d1c
Nikolassantos <nikolassantoshttp@gmail.com>
2020-04-07 15:02:03 -0300

    Refazendo a tela de cadastro

--- src/pages/Internas/CadastrarProdutos/index.js
+++ src/pages/Internas/CadastrarProdutos/index.js
@@ -1,41 +1,48 @@
 import React from 'react';
 
 import {
-  Body,
-  FormBackground,
-  Label,
-  Itens
+  Container,
+  Box,
+  Title,
+  Content,
+  NomedoProduto,
+  Descrição
 
 } from "./styles";
+
+import './styles.css'
 
 
 export default function Cozinha () {
   return(
-    <Body>
-      <FormBackground>
-        <Itens>
-        <div>
+    <Container>
+      <Box>
+        <Title>CADASTRAR PRODUTOS</Title>
+        <Content>
           <div>
-            <input type="file" name="img" id="img"/>  
-          </div>
-          <div>
-            <Label>
-              <label htmlFor="#">Nome do produto</label>
-            </Label>
-            <input type="name"/>
-          </div>
-          <div>
-            <Label>
-              <label htmlFor="#">Descrição</label>
-            </Label>
-            <input type="name"/>
-          </div>
-          <div>
-            <button type="submit">Cadastrar</button>
-          </div>
-        </div>
-        </Itens>
-      </FormBackground>
-    </Body>
+              <input type="file" name="" id=""/>
+            </div>
+            <div>
+              <NomedoProduto>
+                <div>
+                  <label>Nome</label>
+                </div>
+                <input type="text" name="" id=""/>
+              </NomedoProduto>
+            </div>
+            <div>
+              <Descrição>
+                <div>
+                  <label htmlFor="">Descrição</label>
+                </div>
+                <input type="text" name="" id=""/>      
+              </Descrição>
+            </div>
+            <div>
+              <button type="submit">Cadastrar</button>
+            </div>
+        </Content>
+      </Box>
+    </Container>
   );
 }
